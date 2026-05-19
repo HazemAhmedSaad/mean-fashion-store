ab742d21ca3af0e6470296261287fdffe2a1ddf0
HazemAhmedSaad <zozahmed939@gmail.com>
2026-05-19 19:58:03 +0300

    end from testimonials end point

--- server/src/models/testimonials.schema.js
+++ server/src/models/testimonials.schema.js
@@ -1,27 +1,16 @@
 import mongoose from "mongoose";
+
 const testimonialSchema = new mongoose.Schema(
     {
-        // لو المستخدم عامل login
-        user: {
-            type: mongoose.Schema.Types.ObjectId,
-            ref: "User",
-            required: false
-        },
-
-        // لو Guest
         name: {
             type: String,
-            required: function () {
-                return !this.user; // مطلوب لو مفيش user
-            },
+            required: true,
             trim: true
         },
 
         phone: {
             type: String,
-            required: function () {
-                return !this.user;
-            },
+            required: true,
             match: [/^01[0-2,5]{1}[0-9]{8}$/, "Invalid Egyptian phone number"]
         },
 
@@ -36,7 +25,7 @@ const testimonialSchema = new mongoose.Schema(
             type: Number,
             min: 1,
             max: 5,
-            default: 5 // ممكن تخليها optional
+            default: 5
         },
 
         status: {
