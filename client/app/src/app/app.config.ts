2f1c0ada2ffee864e1191d2ba053e20910b8a159
HazemAhmedSaad <zozahmed939@gmail.com>
2026-05-20 20:11:14 +0300

    update folder structure

--- client/app/src/app/app.config.ts
+++ client/app/src/app/app.config.ts
@@ -2,11 +2,15 @@ import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { provideHttpClient, withInterceptors } from '@angular/common/http';
+import { authInterceptor } from './core/interceptors/auth.interceptor';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideRouter(routes),
-  
+    provideHttpClient(
+      withInterceptors([authInterceptor])
+    )
   ]
 };
